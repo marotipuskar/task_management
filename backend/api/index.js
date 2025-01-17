d6866cfbf9004ac06a272ebcd4ccd24b941c42d6
Maroti Puskar <119912274+marotipuskar@users.noreply.github.com>
2025-01-17 17:53:46 +0530

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -6,7 +6,13 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://frontend-task-management-wine.vercel.app/"],
+    methods:["post", "GET"],
+    // credentials: true
+  }
+));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
